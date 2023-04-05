f9e11decb4ec24fbb71bd796332fe6f98d83b34c
Omar Solis <omarsolis@Omars-MacBook-Pro.local>
2023-04-05 11:11:56 -0700

    feat: added list_of_issues endpoint

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,18 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
+
+url_service = "http://app:5000"
+creator_author = 'Createy McCreateFace'
 
 @app.route("/", methods=["GET"])
 def index():
     return "ok"
+
+
+@app.route("/list_of_issues", methods=["GET"])
+def list_of_issues():
+    r = requests.get(url_service + '/issues').json()
+    filtered_list = [i for i in r if i['assignee'] == creator_author]
+    return filtered_list
